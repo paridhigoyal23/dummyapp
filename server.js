94504196a2eb80de52466f77098692c589c64fb4
Paridhi <paridhigoyal23@gmail.com>
2023-08-18 20:51:54 +0530

    minor change in server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var path = require('path');
+const express = require("express");
+const http = require("http");
+const path = require("path");
 
-var app = express();
+const app = express();
 
-app.get('/', (req, res) => {
-   res.sendFile(path.resolve('dist/dummyapp/index.html'))
+app.use(express.static(__dirname + "/dist/dummyapp"));
+
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
+
+const server = http.createServer(app);
+
+server.listen(80, () => {
+  console.log("Server started!");
 });
-
-app.listen(80, () => {
-  console.log('Server started!')
-})
